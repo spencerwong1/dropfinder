147bbc2db5a5bd79986f87f7a08f09ce07f24580
Spencer Wong <z5416662@ad.unsw.edu.au>
2023-07-22 20:26:26 +1000

    html added and has functionability

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const APIController = (function() {
-    
-    const clientId = 'bb7ed959d49c4e4aa37504863df90a38';
+   const clientId = 'bb7ed959d49c4e4aa37504863df90a38';
     const clientSecret = 'd158d2dba8ab4df7b2a6dfd4f805096d';
 
     const _getToken = async() => { 
@@ -37,95 +35,24 @@ const APIController = (function() {
     //     console.log(data);
     // })();
 
+    function getPlaylistId(songLink) {
+        // 2 cases
+        if (songLink.includes("open.spotify.com/")) {
+            // Song Link
+            var split = songLink.split("/")
+            return split[split.length - 1].split("?")[0];
+        } else if (songLink.includes("spotify:track:")) {
+            // Spotify URL
+            return songLink.split(":")[2];
+        }
+    }
+
     async function audioAnalysis(token, id) {
         const result = await fetch(`https://api.spotify.com/v1/audio-analysis/${id}`, {
             method: "GET", headers: { Authorization: `Bearer ${token}` }
         });
         const data = await result.json();
         return data;
-    }
-
-    (async () => {
-        try {
-            // sampling legend by tevvenz as it has an obvious base drop;
-       const id = '05EG9LwFCVjkoYEWzsrHHO?si=bd2e5fafdd3e48be';
-        // sampling too cold by sickmode
-      //  const id = '0vBOyqICis94fkSwJbfUeF?si=6a2d5222010d4390&nd=1';
-        const token = await _getToken();
-        const audio_data = await audioAnalysis(token, id);
-        const data_sections = audio_data.sections;
-    
-        var i = 1;
-        for (const section of data_sections) {
-            const { start, duration, confidence, loudness} = section;
-            console.log(`Section ${i}`);
-            console.log("Start:", start);
-            console.log("Duration:", duration);
-            console.log("Confidence:", confidence);
-            console.log("Loudness:", loudness);
-            console.log(`\n`);
-            i++;
-          }
-      
-          // Other data analysis or processing can be performed here.
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      })();
-      
-
-    // The "sections" array 
-    // in the data provides information about different 
-    // sections of the song, such as the start time, 
-    // duration, and confidence. Look for the section
-    //  with the highest confidence value, as that 
-    //  is more likely to represent the main chorus 
-    //  of the song.
-
-    // Look for Energy Changes: 
-    // The chorus usually has a higher energy level
-    //  and stands out compared to other parts of 
-    //  the song. You can use loudness data to detect 
-    //  energy changes. Check for a significant 
-    //  increase in loudness in the section with 
-    //  the highest confidence.
-
-    // Use the Bars and Beats Data: The "bars" and "beats"
-    //  arrays provide timing information for bars and beats
-    //   in the song. Bars are larger musical phrases, 
-    //   and beats are the individual time units within
-    //    a bar. The chorus often has a consistent pattern
-    //    of bars and beats. Analyze the pattern of bars 
-    //    and beats around the section with the highest
-    //     confidence to see if there's a repeated pattern,
-    //      which could indicate the chorus.
-
-
-
-    const _getPlaylistByGenre = async (token, genreId) => {
-
-        const limit = 10;
-        
-        const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
-        });
-
-        const data = await result.json();
-        return data.playlists.items;
-    }
-
-    const _getTracks = async (token, tracksEndPoint) => {
-
-        const limit = 10;
-
-        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-            method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + token}
-        });
-
-        const data = await result.json();
-        return data.items;
     }
 
     const _getTrack = async (token, trackEndPoint) => {
@@ -139,199 +66,52 @@ const APIController = (function() {
         return data;
     }
 
-    return {
-        getToken() {
-            return _getToken();
-        },
-        getGenres(token) {
-            return _getGenres(token);
-        },
-        getPlaylistByGenre(token, genreId) {
-            return _getPlaylistByGenre(token, genreId);
-        },
-        getTracks(token, tracksEndPoint) {
-            return _getTracks(token, tracksEndPoint);
-        },
-        getTrack(token, trackEndPoint) {
-            return _getTrack(token, trackEndPoint);
-        }
-    }
-})();
 
-
-
-/*
-// UI Module
-const UIController = (function() {
-
-    //object to hold references to html selectors
-    const DOMElements = {
-        selectGenre: '#select_genre',
-        selectPlaylist: '#select_playlist',
-        buttonSubmit: '#btn_submit',
-        divSongDetail: '#song-detail',
-        hfToken: '#hidden_token',
-        divSonglist: '.song-list'
+    async function myFunc() {
+        var input = document.querySelector("#user-input");
+        msg.innerHTML = "hello" + input.value;
     }
 
-    //public methods
-    return {
-
-        //method to get input fields
-        inputField() {
-            return {
-                genre: document.querySelector(DOMElements.selectGenre),
-                playlist: document.querySelector(DOMElements.selectPlaylist),
-                tracks: document.querySelector(DOMElements.divSonglist),
-                submit: document.querySelector(DOMElements.buttonSubmit),
-                songDetail: document.querySelector(DOMElements.divSongDetail)
-            }
-        },
-
-        // need methods to create select list option
-        createGenre(text, value) {
-            const html = `<option value="${value}">${text}</option>`;
-            document.querySelector(DOMElements.selectGenre).insertAdjacentHTML('beforeend', html);
-        }, 
-
-        createPlaylist(text, value) {
-            const html = `<option value="${value}">${text}</option>`;
-            document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML('beforeend', html);
-        },
-
-        // need method to create a track list group item 
-        createTrack(id, name) {
-            const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
-            document.querySelector(DOMElements.divSonglist).insertAdjacentHTML('beforeend', html);
-        },
-
-        // need method to create the song detail
-        createTrackDetail(img, title, artist) {
-
-            const detailDiv = document.querySelector(DOMElements.divSongDetail);
-            // any time user clicks a new song, we need to clear out the song detail div
-            detailDiv.innerHTML = '';
-
-            const html = 
-            `
-            <div class="row col-sm-12 px-0">
-                <img src="${img}" alt="">        
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label for="Genre" class="form-label col-sm-12">${title}:</label>
-            </div>
-            <div class="row col-sm-12 px-0">
-                <label for="artist" class="form-label col-sm-12">By ${artist}:</label>
-            </div> 
-            `;
-
-            detailDiv.insertAdjacentHTML('beforeend', html)
-        },
-
-        resetTrackDetail() {
-            this.inputField().songDetail.innerHTML = '';
-        },
-
-        resetTracks() {
-            this.inputField().tracks.innerHTML = '';
-            this.resetTrackDetail();
-        },
-
-        resetPlaylist() {
-            this.inputField().playlist.innerHTML = '';
-            this.resetTracks();
-        },
+    async function main() {
+        try {
+            var input = document.querySelector("#user-input");
+            msg.innerHTML = "Link: " + input.value;
+            const id = getPlaylistId(input.value);
+            msg2.innerHTML = `ID: ${id}`;
+        // sampling legend by tevvenz as it has an obvious base drop;
+        // const id = '05EG9LwFCVjkoYEWzsrHHO?si=bd2e5fafdd3e48be';
+        // sampling too cold by sickmode
+        // const id = '0vBOyqICis94fkSwJbfUeF?si=6a2d5222010d4390&nd=1';
         
-        storeToken(value) {
-            document.querySelector(DOMElements.hfToken).value = value;
-        },
-
-        getStoredToken() {
-            return {
-                token: document.querySelector(DOMElements.hfToken).value
-            }
+        const token = await _getToken();
+        const audio_data = await audioAnalysis(token, id);
+        const data_sections = audio_data.sections;
+        // var i = 1;
+        var loudest = -999;
+        var time;
+        var assurement;
+        for (const section of data_sections) {
+            const { start, confidence, loudness} = section;
+            // confidence subject to change.
+            if (loudness > loudest && confidence > 0.7) {
+                loudest = loudness;
+                time = start;
+                assurement = confidence * 100;
+            } 
+            // console.log(`Section ${i}`);
+            // console.log("Start:", start);
+            // console.log("Confidence:", confidence);
+            // console.log("Loudness:", loudness);
+            // console.log(`\n`);
+            // i++;
         }
-    }
+        msg3.innerHTML = `BASE DROP IS AT TIME: ${time}`;
+        msg4.innerHTML = `WE KNOW THIS BECAUSE THE VOLUME IS LOUDEST AT ${loudest}`;
+        msg5.innerHTML = `ALSO I AM ${assurement}% SURE HAHA`;
 
-})();
-
-const APPController = (function(UICtrl, APICtrl) {
-
-    // get input field object ref
-    const DOMInputs = UICtrl.inputField();
-
-    // get genres on page load
-    const loadGenres = async () => {
-        //get the token
-        const token = await APICtrl.getToken();           
-        //store the token onto the page
-        UICtrl.storeToken(token);
-        //get the genres
-        const genres = await APICtrl.getGenres(token);
-        //populate our genres select element
-        genres.forEach(element => UICtrl.createGenre(element.name, element.id));
-    }
-
-    // create genre change event listener
-    DOMInputs.genre.addEventListener('change', async () => {
-        //reset the playlist
-        UICtrl.resetPlaylist();
-        //get the token that's stored on the page
-        const token = UICtrl.getStoredToken().token;        
-        // get the genre select field
-        const genreSelect = UICtrl.inputField().genre;       
-        // get the genre id associated with the selected genre
-        const genreId = genreSelect.options[genreSelect.selectedIndex].value;             
-        // ge the playlist based on a genre
-        const playlist = await APICtrl.getPlaylistByGenre(token, genreId);       
-        // create a playlist list item for every playlist returned
-        playlist.forEach(p => UICtrl.createPlaylist(p.name, p.tracks.href));
-    });
-     
-
-    // create submit button click event listener
-    DOMInputs.submit.addEventListener('click', async (e) => {
-        // prevent page reset
-        e.preventDefault();
-        // clear tracks
-        UICtrl.resetTracks();
-        //get the token
-        const token = UICtrl.getStoredToken().token;        
-        // get the playlist field
-        const playlistSelect = UICtrl.inputField().playlist;
-        // get track endpoint based on the selected playlist
-        const tracksEndPoint = playlistSelect.options[playlistSelect.selectedIndex].value;
-        // get the list of tracks
-        const tracks = await APICtrl.getTracks(token, tracksEndPoint);
-        // create a track list item
-        tracks.forEach(el => UICtrl.createTrack(el.track.href, el.track.name))
-        
-    });
-
-    // create song selection click event listener
-    DOMInputs.tracks.addEventListener('click', async (e) => {
-        // prevent page reset
-        e.preventDefault();
-        UICtrl.resetTrackDetail();
-        // get the token
-        const token = UICtrl.getStoredToken().token;
-        // get the track endpoint
-        const trackEndpoint = e.target.id;
-        //get the track object
-        const track = await APICtrl.getTrack(token, trackEndpoint);
-        // load the track details
-        UICtrl.createTrackDetail(track.album.images[2].url, track.name, track.artists[0].name);
-    });    
-
-    return {
-        init() {
-            console.log('App is starting');
-            loadGenres();
+        console.log(time);
+        console.log(loudest);
+        } catch (error) {
+          console.error('Error:', error);
         }
-    }
-
-})(UIController, APIController);
-
-// will need to call a method to load the genres on page load
-APPController.init();
-*/
+      };
